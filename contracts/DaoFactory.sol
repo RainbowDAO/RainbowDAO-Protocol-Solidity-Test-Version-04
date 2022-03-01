871b31b3ab4742b45c410a044f6e28a423bc8bb6
HarrisWongg <harriswong@yeah.net>
2022-03-01 17:41:12 +0800

    a

---
+++ contracts/DaoFactory.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import './DaoManager.sol';
+contract DaoFactory {
+    address[] public daos;
+    function newDao(string memory _name,string memory _desc, string memory _logo,address _govToken, bool _isRainbowToken) returns(address) {
+        DaoManager dao = new DaoManager(_name,_desc,_logo,_govToken,_isRainbowToken);
+        daos.push(address(dao));
+        return address(dao);
+    }
+    // Get the dao counts
+    function getDaoCount() public view returns(uint) {
+        return daos.length();
+    }
+}
---
+++ contracts/DaoManager.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+contract DaoManager {
+    // store the dao base information
+    struct DaoBase{
+        string  name;
+        string  desc;
+        string  logo;
+        address token;
+        bool isRainbowToken; //Is the rainbow token generated
+    }
+    DaoBase public daoBaseInfo;
+    constructor(string memory _name,string memory _desc, string memory _logo,address _govToken, bool _isRainbowToken) public  {
+        daoBaseInfo = DaoBase ({
+            name : _name,
+            desc:_desc,
+            log:_logo,
+            token:_govToken,
+            isRainbowToken:_isRainbowToken
+        });
+    }
+}
