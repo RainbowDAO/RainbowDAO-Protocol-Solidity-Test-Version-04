e07b23a6f549235ffc59608b3b2ee75e42dd12b9
HarrisWongg <harriswong@yeah.net>
2022-03-10 17:21:40 +0800

    add union list

--- contracts/UnionDaoFactory.sol
+++ contracts/UnionDaoFactory.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.0;
 import './UnionDao.sol';
 import './interface/IUnionDaoFactory.sol';
+
 contract UnionDaoFactory is IUnionDaoFactory {
+    address[] public unionDaos;
     function newUnionDao(string memory _name) external override returns(address) {
         UnionDao ud = new UnionDao(msg.sender,_name);
+        unionDaos.push(address(ud));
         return address(ud);
+    }
+    // get the dao count
+    function getUnionDaoLength() public view returns(uint){
+        return unionDaos.length;
     }
 }
