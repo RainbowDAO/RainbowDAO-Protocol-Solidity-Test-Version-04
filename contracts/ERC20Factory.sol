0020a9f94a90ceb9fac6f42d1429428e21ded6ad
HarrisWongg <harriswong@yeah.net>
2022-05-12 11:00:12 +0800

    add erc20 factory

---
+++ contracts/ERC20Factory.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+import './erc20.sol';
+contract ERC20Factory {
+    function newToken(address _manager,uint _totalSupply,string memory _name,string memory _symbol,bool _isMint) public returns(address) {
+        erc20 govToken = new erc20(_manager,_totalSupply,_name,_symbol,_isMint);
+        return address(govToken);
+    }
+}
